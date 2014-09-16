8ab0400f744b633539153a09da8b9c30d792aaef
Dmitry Zuykov <dzuikov@gmail.com>
2014-09-16 20:32:51 +0400

    up

--- xmonad.hs
+++ xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
+import XMonad.Util.SpawnOnce
 
 import Data.List
 
@@ -11,30 +12,34 @@ theConfig = desktopConfig
 
 myTerminal = "lxterminal"
 
+myScreenShotDir = "$HOME/Pictures/Screenshots"
+
+myManageHook = manageDocks <+> manageHook theConfig
+
+myKeys = defineKeys [ ("M-p",        spawn dmenuRun)
+                    , ("<Print>",    spawn sshot)
+                    ]
+  where defineKeys = keys . additionalKeysP theConfig
+
+main = xmonad $ theConfig { modMask    = mod4Mask
+                          , terminal   = myTerminal
+                          , manageHook = myManageHook
+                          , keys       = myKeys
+                          , startupHook = startup
+                          }
+
+startup :: X()
+startup = do
+  return ()
+
 dmenuRun = unwords [ "dmenu_run "
                    , "-nb", "'#e0e0e0'"
                    , "-nf", "'#3b495e'"
                    , "-fn", "'DejaVu Sans Mono-10:style=bold'"
                    ]
 
-myScreenShotDir = "$HOME/Pictures/Screenshots"
-
 sshot = unwords [ "mkdir -p", myScreenShotDir, ";"
                 , "sfile=" ++ myScreenShotDir ++ "/" ++ "`date +'%Y%m%d%H%M%S'`.png", ";"
                 , "import -window root $sfile", ";"
                 , "xdg-open $sfile"
                 ]
-
-myManageHook = manageDocks <+> manageHook theConfig
-
-myKeys = keys $ additionalKeysP theConfig newKeys
-  where newKeys = [ ("M-p",        spawn dmenuRun)
-                  , ("<Print>",    spawn sshot)
-                  ]
-
-main = xmonad $ theConfig { modMask    = mod4Mask
-                          , terminal   = myTerminal
-                          , manageHook = myManageHook
-                          , keys       = myKeys
-                          }
-
